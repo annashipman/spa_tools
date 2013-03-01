b3ae2192fa6818b1e107e1062728edaa49c6334b
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2013-03-01 21:42:05 +0000

    Create file of the email addresses of the submitters

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Tools to help process SPA programme creation
 
-Copy data from this page: http://www.spaconference.org/scripts/proposals.php into data/allproposals.txt then run tools/prune_submissions.sh
+Copy data from this page: http://www.spaconference.org/scripts/proposals.php into data/allproposals.txt and the list of users into data/users.txt, then run tools/prune_submissions.sh
 
 
--- tools/prune_submissions.sh
+++ tools/prune_submissions.sh
@@ -15,5 +15,19 @@ done < data/list_without_change.txt > data/all_on_one_line.txt
 echo "Printing session, presenter and status..."
 awk -F $'\t' '{ print $1 " " $2 $6 }' < data/all_on_one_line.txt > data/relevant_details.txt
 
+echo "Creating list of names..."
+awk -F $'\t' '{ print $2 }' < data/all_on_one_line.txt | sort | uniq > data/names_only.txt
+
+echo "Creating list of names and emails from users.txt"
+awk -F $'\t' '{ print $2 "	" $3 }' < data/users.txt > data/names_and_emails.txt
+
+echo "Getting email addresses of all presenters..."
+while read line
+do
+	grep $line data/names_and_emails.txt
+
+done < data/names_only.txt > data/only_relevant_names_and_emails.txt
+
+
 echo "Tidying up..."
-rm data/list_without_change.txt data/all_on_one_line.txt
+rm data/list_without_change.txt data/all_on_one_line.txt data/names_only.txt data/names_and_emails.txt
