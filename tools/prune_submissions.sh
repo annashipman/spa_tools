eac79d4dde7f4d25108c6e43eac5e09956b0abe4
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2013-03-01 20:54:45 +0000

    Merge session name onto same line as submitter name

--- tools/prune_submissions.sh
+++ tools/prune_submissions.sh
@@ -2,5 +2,12 @@
 
 # prune submissions
 
-echo "Get rid of lines that say change"
+echo "Getting rid of lines that say change..."
 awk '! /^change/ { print }' < data/allproposals.txt > data/list_without_change.txt
+
+echo "Merging session name line with line containing the rest of the details..."
+while read line
+do
+        read line2
+        echo "$line $line2"
+done < data/list_without_change.txt > data/all_on_one_line.txt
