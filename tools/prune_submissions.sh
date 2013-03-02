2adec689f7c94e64c2ba19d487213e42e554da36
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2013-03-02 13:01:28 +0000

    Make all separators tabs

--- tools/prune_submissions.sh
+++ tools/prune_submissions.sh
@@ -13,7 +13,7 @@ do
 done < data/list_without_change.txt > data/all_on_one_line.txt
 
 echo "Printing presenter, session and status in order of presenters..."
-awk -F $'\t' '{ print $2 " " $1 $6 }' < data/all_on_one_line.txt | sort > data/relevant_details.txt
+awk -F $'\t' '{ print $2 "	" $1 "	" $6 }' < data/all_on_one_line.txt | sort > data/relevant_details.txt
 
 echo "Creating list of names and emails from users.txt"
 awk -F $'\t' '{ print $2 "	" $3 }' < data/users.txt > data/names_and_emails.txt
@@ -22,7 +22,7 @@ echo "Concatenating email addresses with other data..."
 while read name rest
 do
 	name_plus_email=$(grep -w $name data/names_and_emails.txt)
-	echo "$name_plus_email "	" $rest"
+	echo "$name_plus_email "$'\t'" $rest"
 
 done < data/relevant_details.txt > data/concatenated_list.txt
 
