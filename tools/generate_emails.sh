c14f70ca5b12342e085d106c58b969a8eae02d6d
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2013-03-03 15:33:17 +0000

    Generate the different emails

--- tools/generate_emails.sh
+++ tools/generate_emails.sh
@@ -4,5 +4,18 @@ IFS=$'\t'
 
 while read name email session status
 do  
-	emails/generate_rejection_email.sh "$name" "$session"
-done < output/concatenated_list.txt
+	if [ " Accepted" = $status ]; then
+		subject="SPA 2013 session accepted"
+		emails/generate_acceptance_email.sh "$subject" "$email" "$name" "$session"
+	elif [ " Reserve" = $status ]; then
+		subject="SPA 2013 session on reserve list"
+		emails/generate_reserve_email.sh "$subject" "$email" "$name" "$session"
+	elif [ " Rejected" = $status ]; then
+		subject="SPA 2013 session rejected"
+		emails/generate_rejection_email.sh "$subject" "$email" "$name" "$session"
+	fi
+	echo " "
+	echo "------cut here -------"
+	echo " "
+done < output/concatenated_list.txt > output/emails.txt
+
