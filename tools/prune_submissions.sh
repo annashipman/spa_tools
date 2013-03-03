3b7cb3120e322ac854da6cd7b25b4e18961e6119
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2013-03-03 14:20:42 +0000

    Generated files go to output folder instead of data

--- tools/prune_submissions.sh
+++ tools/prune_submissions.sh
@@ -3,29 +3,29 @@
 # prune submissions
 
 echo "Getting rid of lines that say change..."
-awk '! /^change/ { print }' < data/allproposals.txt > data/list_without_change.txt
+awk '! /^change/ { print }' < data/allproposals.txt > output/list_without_change.txt
 
 echo "Merging session name line with line containing the rest of the details..."
 while read line
 do
         read line2
         echo "$line	$line2"
-done < data/list_without_change.txt > data/all_on_one_line.txt
+done < output/list_without_change.txt > output/all_on_one_line.txt
 
 echo "Printing presenter, session and status in order of presenters..."
-awk -F $'\t' '{ print $2 "	" $1 "	" $6 }' < data/all_on_one_line.txt | sort > data/relevant_details.txt
+awk -F $'\t' '{ print $2 "	" $1 "	" $6 }' < output/all_on_one_line.txt | sort > output/relevant_details.txt
 
 echo "Creating list of names and emails from users.txt"
-awk -F $'\t' '{ print $2 "	" $3 }' < data/users.txt > data/names_and_emails.txt
+awk -F $'\t' '{ print $2 "	" $3 }' < data/users.txt > output/names_and_emails.txt
 
 echo "Concatenating email addresses with other data..."
 while read name rest
 do
-	name_plus_email=$(grep -w $name data/names_and_emails.txt)
+	name_plus_email=$(grep -w $name output/names_and_emails.txt)
 	echo "$name_plus_email "$'\t'" $rest"
 
-done < data/relevant_details.txt > data/concatenated_list.txt
+done < output/relevant_details.txt > output/concatenated_list.txt
 
 
 echo "Tidying up..."
-rm data/list_without_change.txt data/all_on_one_line.txt data/names_and_emails.txt data/relevant_details.txt
+rm output/list_without_change.txt output/all_on_one_line.txt output/names_and_emails.txt output/relevant_details.txt
