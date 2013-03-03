06e2d19f05a05ae13d1e3d73ffa774ea82c02c44
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2013-03-03 15:46:46 +0000

    Remove leading space from session and status

--- tools/generate_emails.sh
+++ tools/generate_emails.sh
@@ -4,13 +4,16 @@ IFS=$'\t'
 
 while read name email session status
 do  
-	if [ " Accepted" = $status ]; then
+	session=$(echo $session | sed 's/^ *//g')
+	status=$(echo $status | sed 's/^ *//g')
+
+	if [ "Accepted" = $status ]; then
 		subject="SPA 2013 session accepted"
 		emails/generate_acceptance_email.sh "$subject" "$email" "$name" "$session"
-	elif [ " Reserve" = $status ]; then
+	elif [ "Reserve" = $status ]; then
 		subject="SPA 2013 session on reserve list"
 		emails/generate_reserve_email.sh "$subject" "$email" "$name" "$session"
-	elif [ " Rejected" = $status ]; then
+	elif [ "Rejected" = $status ]; then
 		subject="SPA 2013 session rejected"
 		emails/generate_rejection_email.sh "$subject" "$email" "$name" "$session"
 	fi
